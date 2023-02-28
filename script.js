ebbd1a68bab4cecff4a5efc3da931eba9378047d
ThibautMilville <tmilville.pro@gmail.com>
2023-02-28 21:14:45 +0100

    added_features

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var addToDoButton = document.getElementById('addToDo');
 var toDoContainer = document.getElementById('toDoContainer');
-var addToDoButton = document.getElementById('inputField');
+var inputField = document.getElementById('inputField');
 
 addToDoButton.onclick = function() {
     // Check if the input is not empty
-    if(inputField.value != "") {
+    if(inputField.value != '') {
         // Creation of a paragraph if input is not empty
-        var paragraph = document.createElement('p')
+        var paragraph = document.createElement('p');
     }
     // Put in this paragraph the content of the input
     paragraph.innerText = inputField.value;
@@ -16,4 +16,12 @@ addToDoButton.onclick = function() {
     toDoContainer.appendChild(paragraph);
     // Make the input empty when the paragraph is added
     inputField.value = '';
+    // Cross out the paragraph when it is clicked
+    paragraph.addEventListener('click', function(){
+        paragraph.classList.add('paragraph_click');
+    })
+    // Delete the task when clicked
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
 }
